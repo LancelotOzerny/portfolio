eacdc89bb2239c5669fa6cf0c5911e10b9e1d8c0
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-08-02 02:50:14 +0300

    Текстовые изменения

--- App/Configs/App/networks.php
+++ App/Configs/App/networks.php
@@ -1,26 +1,26 @@
 <?php
 return [
-	[
+	0 => [
 		'name' => 'ВКонтакте',
 		'link' => 'https://vk.com/lancy2002',
 		'icon' => 'vk',
 	],
-	[
+	1 => [
 		'name' => 'LinkedIn',
-		'link' => 'https://www.linkedin.com/',
+		'link' => 'https://www.linkedin.com/in/максим-беляков-9389813bb/',
 		'icon' => 'linkedin',
 	],
-	[
+	2 => [
 		'name' => 'GitHub',
 		'link' => 'https://github.com/LancelotOzerny/',
 		'icon' => 'github',
 	],
-	[
+	3 => [
 		'name' => 'Max',
-		'link' => 'https://max.ru/',
+		'link' => 'https://max.ru/u/f9LHodD0cOKV7VTyRLUNld_r6IMLrC2zLjRqUwKVyryg83dCpbhE7zKK25Q',
 		'icon' => 'max',
 	],
-	[
+	4 => [
 		'name' => 'Telegram',
 		'link' => 'https://t.me/lalalancy',
 		'icon' => 'telegram',
